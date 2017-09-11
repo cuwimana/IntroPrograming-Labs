32d1d543192199fd07539bfea35f316f7f7bf3c2
cuwimana <charlotte.uwimana1@notes.marist.edu>
2017-09-10 22:56:39 -0400

    Solution to the Mad Libs exercises

---
+++ madlib.py
@@ -0,0 +1,11 @@
+noun = input("Enter a Noun: ")
+print("\n")
+verb = input("Enter a verb: ")
+print("\n")
+adjective = input("Enter an adjective: ")
+print("\n")
+place = input("Enter a place: ")
+print("\n")
+
+
+print("Take your "+adjective+" "+noun+" and "+verb+" it to the "+place+"!")      
