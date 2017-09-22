11d294d417a6e9ffdde93fe32111b21e1ffd0dcd
cuwimana <charlotte.uwimana1@notes.marist.edu>
2017-09-22 12:25:25 -0400

    Usability improvemenents to the guessing game, resolves #2 and #3.

--- guessing-game.py
+++ guessing-game.py
@@ -7,11 +7,14 @@
 animal_Name = "dog"
 while True: 
     print(" This program is thinking of an animal")
-    guess = input (" guess the name of the animal: ")
+    guess = input(" guess the name of the animal: ").lower()
     if animal_Name == guess: 
         print("congratulation, you got the name!!!!")
+    elif guess == "quit":
+        break
     else:
         print( "Ooops!! The answer is wrong. Try again. ")
+        
     
     
 
