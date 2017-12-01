b7b61a23e124799d826a89582acd0ba25e88f231
cuwimana <charlotte.uwimana1@notes.marist.edu>
2017-12-01 13:19:14 -0500

    Add Entry command window

--- ArithmaticEngine.py
+++ ArithmaticEngine.py
@@ -12,47 +12,68 @@
 #================================================================
 
 from graphics import *
-#def intro():
-    #print("Welcome to Arithematic Engine!\n"
-          #"===============================\n")
-    #print("This program is allow you to enter two numbers and choose the operation that you want to use.")
-    #print()
+def intro():
+    app = GraphWin("Arthimatic Angine", 600, 200)
+    app.setCoords(0,0,10,10)
+    Text(Point(4,7), "Welcome to Arithmatic Engine!\n"
+                   "===============================\n").draw(app)
+    Text(Point(4,3), "This program is allow you to enter two numbers\n"
+                     "and choose the operation that you want to use\n.").draw(app)
+    Text(Point(4,1), " The valid command are: add, mult, diff, quot, and quit").draw(app)
+
+    inputBox = Entry(Point(8,1), 6).draw(app)
+    while app.getKey() != "Return": pass
+    cmd = inputBox.getText()
+    app.close()
+    return cmd
+
+def getData(cmd):
+    app = GraphWin("Arthmatic Engine", 600, 200)
+    app.setCoords (0,0,10,10)
+
+    Text(Point(5,6), " Enter two numbers. ").draw(app)
+
+    num1 = Entry(Point(8,3), 6).draw(app)
+    num2 = Entry(Point(8,1), 6).draw(app)
+
+    while app.getKey()!= "Return": pass
+    num1 = float(num1.getText())
+    num2= float(num2.getText())
+  
+    if cmd == "add":
+        result = num1 + num2
+    elif cmd == "mult":
+        result = num1 * num2
+    elif cmd == "diff":
+        result = num1 - num2
+    elif cmd == "quot":
+        result = num1 // num2
+    else:
+        pass
+    Text(Point(5,4), ("The result is: " + str(result)+ ".\n")).draw(app)
+    if app.getKey() == "Return":
+        app.close()
+    
+def loop():
+    cmd = intro()
+    while cmd != "quit":
+        if cmd in ["add", "diff", "mult", "quot"]:
+            try:
+                getData(cmd)
+            except ValueError:
+                print("ERROR: You must enter valid integers!\n")
+                continue
+            except ZeroDivisionError:
+                print("You can not divide by zero.")
+        else:
+            print(" you have entered an invalid command.")
+            return    
+   
 def end():
     print("\n Thank you for using Arithematic Engine...")
     print("\n please come back again soon!")
     
-def loop():
-    print("Valid command are: add, mult, diff, quot, and quit")
-    while True:
-        cmd = input("what the operation you want to perform?: ")
-        if cmd == "quit":
-            break
-        try:
-            a = int(input("Enter the first number: "))
-            b = int(input ( "Enter the second number: "))
-        except:
-            print(" ERROR: you must enter valid integers!\n")
-              
-        if cmd == "add":
-            result = a + b
-        elif cmd == "mult":
-            result = a * b
-        
-        elif cmd == "diff":
-            result = a - b
-        elif cmd == "quot":
-            result = a // b
-        else:
-            raise Exception( " ERROR: Invalid comand '" + cmd + " '! Exitings...") 
- 
-        print( "The result is: " + str(result)+ ".\n")
-def main():
-    app = GraphWin("Arthimatic Angine", 600, 200)
-    app.setCoords(0,0,10,10)
-    Text(Point(5,7), "Welcome to Arithematic Engine!\n"
-                   "===============================\n")
-    Text(Point(4,3), "This program is allow you to enter two numbers and choose the operation that you want to use.")
-   
+def main():  
     loop()
     end()
 main()
