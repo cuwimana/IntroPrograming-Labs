02fa0224078e96b9551f3d9598ce7a24de83022c
cuwimana <charlotte.uwimana1@notes.marist.edu>
2017-12-06 21:04:50 -0500

    Final project Lab activity #10

--- ArithmaticEngine.py
+++ ArithmaticEngine.py
@@ -31,10 +31,10 @@ def getData(cmd):
     app = GraphWin("Arthmatic Engine", 600, 200)
     app.setCoords (0,0,10,10)
 
-    Text(Point(5,6), " Enter two numbers. ").draw(app)
-
-    num1 = Entry(Point(8,3), 6).draw(app)
-    num2 = Entry(Point(8,1), 6).draw(app)
+    Text(Point(5,6), " Enter first number. ").draw(app)
+    num1 = Entry(Point(8,6), 6).draw(app)
+    Text(Point(5,4), " Enter the second number. ").draw(app)
+    num2 = Entry(Point(8,4), 6).draw(app)
 
     while app.getKey()!= "Return": pass
     num1 = float(num1.getText())
@@ -50,24 +50,31 @@ def getData(cmd):
         result = num1 // num2
     else:
         pass
-    Text(Point(5,4), ("The result is: " + str(result)+ ".\n")).draw(app)
+    Text(Point(5,1), ("The result is: " + str(result)+ ".\n")).draw(app).setTextColor("blue")
     if app.getKey() == "Return":
         app.close()
     
 def loop():
+    app = GraphWin("Arthamatic Engine", 600,200)
+    app.setCoords(0,0,10,10)
     cmd = intro()
     while cmd != "quit":
         if cmd in ["add", "diff", "mult", "quot"]:
             try:
                 getData(cmd)
             except ValueError:
-                print("ERROR: You must enter valid integers!\n")
+                error = Text(Point(5,6),"ERROR: You must enter valid integers!\n").draw(app)
+                error.setTextColor("green")
                 continue
             except ZeroDivisionError:
-                print("You can not divide by zero.")
+                error = Text(Point(5,6),"You can not divide by zero.").draw(app)
+                error.setTextColor("red")
         else:
-            print(" you have entered an invalid command.")
-            return    
+            error = Text(Point(5,6)," you have entered an invalid command.").draw(app)
+            error.setTextColor("blue")
+            return
+        if app.getKey() == "Return":
+            app.close()
    
 def end():
     print("\n Thank you for using Arithematic Engine...")
